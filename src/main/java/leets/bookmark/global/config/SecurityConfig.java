917eee54d0d0b7fe54c5dd5822e7d5b40f148526
SeolA-Lee <dlcodus1201@naver.com>
2025-07-06 20:03:40 +0900

    feat: SecurityConfig에 jwt filter 추가

--- src/main/java/leets/bookmark/global/config/SecurityConfig.java
+++ src/main/java/leets/bookmark/global/config/SecurityConfig.java
@@ -1,5 +1,8 @@
 package leets.bookmark.global.config;
 
+import leets.bookmark.global.auth.jwt.filter.JwtAuthenticationFilter;
+import leets.bookmark.global.auth.jwt.service.CustomUserDetailsService;
+import leets.bookmark.global.auth.jwt.service.JwtProvider;
 import leets.bookmark.global.auth.oauth2.service.CustomOAuth2UserService;
 import leets.bookmark.global.auth.oauth2.handler.OAuth2SuccessHandler;
 import lombok.RequiredArgsConstructor;
@@ -9,6 +12,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -17,6 +21,8 @@ public class SecurityConfig {
 
     private final CustomOAuth2UserService customOAuth2UserService;
     private final OAuth2SuccessHandler oAuth2SuccessHandler;
+    private final JwtProvider jwtProvider;
+    private final CustomUserDetailsService userDetailsService;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -29,6 +35,9 @@ public class SecurityConfig {
                                 .requestMatchers("/oauth/**", "/auth/login/**").permitAll()
                                 .requestMatchers("/v3/api-docs", "/v3/api-docs/**", "/swagger-ui.html", "/swagger-ui/**", "/swagger/**").permitAll()
                                 .anyRequest().authenticated()
+                )
+                .addFilterBefore(
+                        new JwtAuthenticationFilter(jwtProvider, userDetailsService), UsernamePasswordAuthenticationFilter.class
                 )
                 .oauth2Login(oauth2 -> oauth2
                                 .authorizationEndpoint(endpoint -> endpoint.baseUri("/oauth2/authorize"))
