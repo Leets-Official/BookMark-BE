3bc2c0e250ca2529e3d99a907f0de40a046a527c
SeolA-Lee <dlcodus1201@naver.com>
2025-07-10 13:05:50 +0900

    refactor: @CurrentUser를 통해 유저 추출

--- src/main/java/leets/bookmark/domain/user/presentation/UserController.java
+++ src/main/java/leets/bookmark/domain/user/presentation/UserController.java
@@ -4,10 +4,10 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import leets.bookmark.domain.user.application.dto.response.UserInfoResponse;
 import leets.bookmark.domain.user.application.usecase.UserUseCase;
-import leets.bookmark.global.auth.jwt.service.CustomUserDetails;
+import leets.bookmark.domain.user.domain.entity.User;
+import leets.bookmark.global.auth.annotation.CurrentUser;
 import leets.bookmark.global.common.response.CommonResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,8 +22,8 @@ public class UserController {
 
     @GetMapping("/me")
     @Operation(summary = "내 정보 조회 API", description = "JWT로 인증된 사용자 본인의 정보를 반환합니다.")
-    public CommonResponse<UserInfoResponse> getUserInfo(@AuthenticationPrincipal CustomUserDetails userDetails) {
-        UserInfoResponse response = userUseCase.getUserInfo(userDetails.getUser());
+    public CommonResponse<UserInfoResponse> getUserInfo(@CurrentUser User user) {
+        UserInfoResponse response = userUseCase.getUserInfo(user);
         return CommonResponse.createSuccess("정보 조회에 성공했습니다.", response);
     }
 }
