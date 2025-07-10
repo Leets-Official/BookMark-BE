7bdbf1b5c5dd82ccbf55bec722f67d3effe32c4b
choes0101 <166037587+choes0101@users.noreply.github.com>
2025-07-11 03:00:11 +0900

    refactor: bookmark 엔티티 JPA 설정

--- src/main/java/leets/bookmark/domain/bookmark/domain/entity/Bookmark.java
+++ src/main/java/leets/bookmark/domain/bookmark/domain/entity/Bookmark.java
@@ -1,13 +1,27 @@
 package leets.bookmark.domain.bookmark.domain.entity;
 
-import leets.bookmark.global.common.entity.BaseTimeEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
-public class Bookmark extends BaseTimeEntity {
+@NoArgsConstructor // JPA는 기본 생성자 필요
+@Entity
+@Table(name = "bookmark")
+public class Bookmark {
+
+    @Id
+    @Column(name = "bookmark_id")
     private Long id;
+
+    @Column(name = "title")
     private String title;
+
+    @Column(name = "thumbnail_url")
     private String thumbnailUrl;
 }
