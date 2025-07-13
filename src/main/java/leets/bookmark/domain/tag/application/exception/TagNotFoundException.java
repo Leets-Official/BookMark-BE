4c602bf6876e4341ca5e903e303ed1e4968bfa5b
SeolA-Lee <dlcodus1201@naver.com>
2025-07-13 14:08:02 +0900

    refactor: 태그 예외 처리 enum 관리로 변경

--- src/main/java/leets/bookmark/domain/tag/application/exception/TagNotFoundException.java
+++ src/main/java/leets/bookmark/domain/tag/application/exception/TagNotFoundException.java
@@ -4,6 +4,6 @@ import leets.bookmark.global.common.exception.BusinessException;
 
 public class TagNotFoundException extends BusinessException {
     public TagNotFoundException() {
-        super(404, "해당 태그가 존재하지 않습니다.");
+        super(TagErrorCode.TAG_NOT_FOUND_EXCEPTION);
     }
 }
