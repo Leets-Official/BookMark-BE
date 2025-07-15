1133c29c60f4d36ad65eec50fdbd7ba7c4a39bd2
choes0101 <166037587+choes0101@users.noreply.github.com>
2025-07-16 03:07:01 +0900

    refactor: preview API @Operation 변경

--- src/main/java/leets/bookmark/domain/bookmark/presentation/BookmarkPreviewController.java
+++ src/main/java/leets/bookmark/domain/bookmark/presentation/BookmarkPreviewController.java
@@ -14,8 +14,12 @@ public class BookmarkPreviewController {
     private final GetBookmarkPreviewUseCase getBookmarkPreviewUseCase;
 
     @Operation(
-        summary = "북마크 미리보기 조회",
-        description = "bookmarkId를 기반으로 해당 북마크의 썸네일과 제목 정보를 반환합니다. "
+        summary = "북마크 미리보기 단건 조회 API",
+        description = """
+        특정 bookmarkId를 기반으로 해당 북마크의 썸네일과 제목을 반환합니다.
+        이 API는 bookmarkId 하나에 대해서만 조회하며,
+        응답은 BookmarkPreviewResponse 단일 객체로 반환됩니다.
+    """
     )
     @GetMapping("/preview")
     public BookmarkPreviewResponse getBookmarkPreview(@RequestParam("id") Long bookmarkId) {
