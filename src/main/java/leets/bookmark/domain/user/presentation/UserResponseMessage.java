8342fdd33235b587b496b3d3e064ee2087c84a41
SeolA-Lee <dlcodus1201@naver.com>
2025-07-15 12:01:29 +0900

    chore: 불필요한 주석 제거

--- src/main/java/leets/bookmark/domain/user/presentation/UserResponseMessage.java
+++ src/main/java/leets/bookmark/domain/user/presentation/UserResponseMessage.java
@@ -6,7 +6,7 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum UserResponseMessage {
-    // UserController 관련
+
     GET_USER_INFO_SUCCESS("정보 조회에 성공했습니다.");
 
     private final String message;
