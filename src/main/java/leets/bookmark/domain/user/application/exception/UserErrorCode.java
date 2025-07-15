f49e5f4e2ea4089a13f1db2720683b27eb9ac3cb
SeolA-Lee <dlcodus1201@naver.com>
2025-07-16 00:59:43 +0900

    chore: errorCode 404로 수정

--- src/main/java/leets/bookmark/domain/user/application/exception/UserErrorCode.java
+++ src/main/java/leets/bookmark/domain/user/application/exception/UserErrorCode.java
@@ -4,13 +4,11 @@ import leets.bookmark.global.common.exception.ErrorCode;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
-import static org.springframework.http.HttpStatus.*;
-
 @Getter
 @RequiredArgsConstructor
 public enum UserErrorCode implements ErrorCode {
 
-    USER_NOT_FOUND_EXCEPTION(NOT_FOUND.value(),"해당 사용자가 존재하지 않습니다.");
+    USER_NOT_FOUND_EXCEPTION(404,"해당 사용자가 존재하지 않습니다.");
 
     private final int errorCode;
     private final String message;
