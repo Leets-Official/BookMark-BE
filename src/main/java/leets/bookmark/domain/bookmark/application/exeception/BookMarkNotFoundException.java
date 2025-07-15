49c95f1e3afbf3f6ea11fa9f96e5e0b8a4200263
choes0101 <166037587+choes0101@users.noreply.github.com>
2025-07-15 18:50:36 +0900

    refactor: BookMarkNotFoundException ErrorCode 통일

--- src/main/java/leets/bookmark/domain/bookmark/application/exeception/BookMarkNotFoundException.java
+++ src/main/java/leets/bookmark/domain/bookmark/application/exeception/BookMarkNotFoundException.java
@@ -1,9 +1,10 @@
 package leets.bookmark.domain.bookmark.application.exeception;
 
 import leets.bookmark.global.common.exception.BusinessException;
+import leets.bookmark.global.common.exception.ErrorCode;
 
 public class BookMarkNotFoundException extends BusinessException {
     public BookMarkNotFoundException() {
-        super(BookmarkErrorCode.NOT_FOUND.getStatus(), BookmarkErrorCode.NOT_FOUND.getMessage());
+        super(ErrorCode.BOOKMARK_NOT_FOUND.getStatusCode(), ErrorCode.BOOKMARK_NOT_FOUND.getMessage());
     }
 }
