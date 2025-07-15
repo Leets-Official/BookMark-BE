e08bd4f95586726d160bd3f418b6161c0b8cb366
choes0101 <166037587+choes0101@users.noreply.github.com>
2025-07-15 16:10:45 +0900

    refactor: 검색히스토리 JPA 설정

--- src/main/java/leets/bookmark/domain/searchhistory/domain/entity/SearchHistory.java
+++ src/main/java/leets/bookmark/domain/searchhistory/domain/entity/SearchHistory.java
@@ -1,15 +1,28 @@
 package leets.bookmark.domain.searchhistory.domain.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import leets.bookmark.global.common.entity.BaseTimeEntity;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-
-import java.time.LocalDateTime;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "search_history")
 public class SearchHistory extends BaseTimeEntity {
+
+    @Id
+    @Column(name = "search_history_id")
     private Long id;
+
+    @Column(name = "user_id")
     private Long userId;
+
+    @Column(name = "keyword")
     private String keyword;
 }
