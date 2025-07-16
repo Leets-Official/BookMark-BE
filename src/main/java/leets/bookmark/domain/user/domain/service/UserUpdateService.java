f8a339e09def1089c180a4d5d0ee4ea1b0bfaafa
SeolA-Lee <dlcodus1201@naver.com>
2025-07-16 13:16:44 +0900

    chore: 불필요한 로그 삭제

--- src/main/java/leets/bookmark/domain/user/domain/service/UserUpdateService.java
+++ src/main/java/leets/bookmark/domain/user/domain/service/UserUpdateService.java
@@ -2,10 +2,8 @@ package leets.bookmark.domain.user.domain.service;
 
 import leets.bookmark.domain.user.domain.entity.User;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-@Slf4j
 @Service
 @RequiredArgsConstructor
 public class UserUpdateService {
