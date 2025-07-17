2a8f83081e288492afb68ebbed6a823fb0743345
choes0101 <166037587+choes0101@users.noreply.github.com>
2025-07-17 21:46:20 +0900

    refactor: SearchHistory (access = AccessLevel.PROTECTED) 추가

--- src/main/java/leets/bookmark/domain/searchhistory/domain/entity/SearchHistory.java
+++ src/main/java/leets/bookmark/domain/searchhistory/domain/entity/SearchHistory.java
@@ -5,13 +5,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import leets.bookmark.global.common.entity.BaseTimeEntity;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 @Table(name = "search_historys")
 public class SearchHistory extends BaseTimeEntity {
