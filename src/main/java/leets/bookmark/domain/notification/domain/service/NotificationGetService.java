89a6f31052bc0ce401e58193d0d43c65000e331b
jj <syeonie0526@gmail.com>
2025-07-19 01:35:42 +0900

    feat: notificationId로 알림 단일 조회 메서드 findById 추가

---
+++ src/main/java/leets/bookmark/domain/notification/domain/service/NotificationGetService.java
@@ -0,0 +1,19 @@
+package leets.bookmark.domain.notification.domain.service;
+
+import leets.bookmark.domain.notification.application.exception.NotificationNotFoundException;
+import leets.bookmark.domain.notification.domain.entity.Notification;
+import leets.bookmark.domain.notification.domain.repository.NotificationRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class NotificationGetService {
+
+    private final NotificationRepository notificationRepository;
+
+    public Notification findById(long notificationId){
+        return notificationRepository.findById(notificationId)
+                .orElseThrow(NotificationNotFoundException::new);
+    }
+}
