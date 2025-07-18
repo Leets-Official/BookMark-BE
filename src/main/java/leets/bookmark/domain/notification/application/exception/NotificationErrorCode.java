528fd395a7b6154167778854eb91f448a38246d8
jj <syeonie0526@gmail.com>
2025-07-19 01:36:06 +0900

    feat: 알림 삭제 기능 추가

---
+++ src/main/java/leets/bookmark/domain/notification/application/exception/NotificationErrorCode.java
@@ -0,0 +1,16 @@
+package leets.bookmark.domain.notification.application.exception;
+
+import leets.bookmark.global.common.exception.ErrorCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum NotificationErrorCode implements ErrorCode {
+
+    NOTIFICATION_NOT_FOUND(404,"해당 알림이 존재하지 않습니다."),
+    NOTIFICATION_OWNER_MISMATCH(403, "해당 알림 소유자만 접근 가능합니다.");
+
+    private final int errorCode;
+    private final String message;
+}
--- src/main/java/leets/bookmark/domain/notification/application/usecase/NotificationUseCase.java
+++ src/main/java/leets/bookmark/domain/notification/application/usecase/NotificationUseCase.java
@@ -1,8 +1,11 @@
 package leets.bookmark.domain.notification.application.usecase;
 
 import leets.bookmark.domain.notification.application.dto.NotificationSaveRequest;
+import leets.bookmark.domain.notification.application.exception.NotificationOwnerMismatchException;
 import leets.bookmark.domain.notification.application.mapper.NotificationMapper;
 import leets.bookmark.domain.notification.domain.entity.Notification;
+import leets.bookmark.domain.notification.domain.service.NotificationDeleteService;
+import leets.bookmark.domain.notification.domain.service.NotificationGetService;
 import leets.bookmark.domain.notification.domain.service.NotificationSaveService;
 import leets.bookmark.domain.user.domain.entity.User;
 import lombok.RequiredArgsConstructor;
@@ -12,7 +15,9 @@ import org.springframework.stereotype.Service;
 @Service
 public class NotificationUseCase {
 
+    private final NotificationGetService notificationGetService;
     private final NotificationSaveService notificationSaveService;
+    private final NotificationDeleteService notificationDeleteService;
 
     private final NotificationMapper notificationMapper;
 
@@ -21,4 +26,18 @@ public class NotificationUseCase {
 
         notificationSaveService.save(notification);
     }
+
+    public void deleteNotification(User user, long notificationId) {
+        Notification notification = notificationGetService.findById(notificationId);
+        validateNotificationOwner(user, notification);
+
+        notificationDeleteService.delete(notification);
+    }
+
+    private void validateNotificationOwner(User user, Notification notification){
+        if(!user.equals(notification.getUser())){
+            throw new NotificationOwnerMismatchException();
+        }
+    }
+
 }
