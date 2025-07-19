64d6f659e43f8124e2c78fe64dc9bae2ca32694e
katejhee <katekwak1@gachon.ac.kr>
2025-07-19 23:10:20 +0900

    feat: 북마크 필터링 기능을 위한 @Query 기반 findAllWithFilter 구현

--- src/main/java/leets/bookmark/domain/bookmark/domain/repository/BookmarkRepository.java
+++ src/main/java/leets/bookmark/domain/bookmark/domain/repository/BookmarkRepository.java
@@ -2,10 +2,21 @@ package leets.bookmark.domain.bookmark.domain.repository;
 
 import leets.bookmark.domain.bookmark.domain.entity.Bookmark;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface BookmarkRepository extends JpaRepository<Bookmark, Long> {
     List<Bookmark> findByMemoContaining(String keyword);
-    List<Bookmark> findAllWithFilter(Long categoryId, List<String> tagNames);
+
+    @Query("""
+    SELECT DISTINCT b FROM Bookmark b
+    JOIN b.categories c
+    JOIN leets.bookmark.domain.tag.domain.entity.Tag t
+    ON t.categoryId = c.id
+    WHERE (:categoryId IS NULL OR c.id = :categoryId)
+    AND (:tagNames IS NULL OR t.tagName IN :tagNames)
+    """)
+    List<Bookmark> findAllWithFilter(@Param("categoryId") Long categoryId, @Param("tagNames") List<String> tagNames);
 }
