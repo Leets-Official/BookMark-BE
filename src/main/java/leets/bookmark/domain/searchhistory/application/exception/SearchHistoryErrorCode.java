9c1b67c1ddb3abe812f5f946d4435c3d42e17fe5
choes0101 <166037587+choes0101@users.noreply.github.com>
2025-07-20 14:30:24 +0900

    refactor: SearchHistoryErrorCode 수정

--- src/main/java/leets/bookmark/domain/searchhistory/application/exception/SearchHistoryErrorCode.java
+++ src/main/java/leets/bookmark/domain/searchhistory/application/exception/SearchHistoryErrorCode.java
@@ -8,7 +8,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum SearchHistoryErrorCode implements ErrorCode {
 
-    SEARCH_HISTORY_NOT_FOUND_EXCEPTION(404,"해당하는 파일을 찾을 수 없습니다.");
+    SEARCH_HISTORY_NOT_FOUND_EXCEPTION(404,"해당하는 검색 기록을 찾을 수 없습니다.");
 
     private final int errorCode;
     private final String message;
