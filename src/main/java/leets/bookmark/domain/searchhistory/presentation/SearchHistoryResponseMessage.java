dd0e883e5924e48e6b1d2abae1d74c07e620063d
choes0101 <166037587+choes0101@users.noreply.github.com>
2025-07-20 14:02:34 +0900

    refactor: SearchHistoryResponseMessage Enum 이름 변경

--- src/main/java/leets/bookmark/domain/searchhistory/presentation/SearchHistoryResponseMessage.java
+++ src/main/java/leets/bookmark/domain/searchhistory/presentation/SearchHistoryResponseMessage.java
@@ -7,9 +7,9 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum SearchHistoryResponseMessage {
 
-    GET_SUCCESS("검색 기록 조회에 성공했습니다."),
-    DELETE_SUCCESS("검색 기록 삭제에 성공했습니다."),
-    CREATE_SUCCESS("검색 기록 생성에 성공했습니다.");
+    GET_SEARCH_HISTORY_SUCCESS("검색 기록 조회에 성공했습니다."),
+    DELETE_SEARCH_HISTORY_SUCCESS("검색 기록 삭제에 성공했습니다."),
+    CREATE_SEARCH_HISTORY_SUCCESS("검색 기록 생성에 성공했습니다.");
 
     private final String message;
 
