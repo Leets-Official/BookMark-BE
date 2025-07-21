b06c3e2c30a2a57ebdbd87c89722d26367b88540
SeolA-Lee <dlcodus1201@naver.com>
2025-07-21 10:56:47 +0900

    fix: 토큰 재발급 API에  @Validated 추가하여 검증되도록 수정

--- src/main/java/leets/bookmark/global/auth/presentation/AuthController.java
+++ src/main/java/leets/bookmark/global/auth/presentation/AuthController.java
@@ -6,6 +6,7 @@ import leets.bookmark.global.auth.jwt.application.dto.JwtTokenReissueRequest;
 import leets.bookmark.global.auth.jwt.service.JwtService;
 import leets.bookmark.global.common.response.CommonResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,7 +23,7 @@ public class AuthController {
     private final JwtService jwtService;
 
     @PostMapping("/reissue")
-    public CommonResponse<JwtTokenDto> reissue(@RequestBody JwtTokenReissueRequest request) {
+    public CommonResponse<JwtTokenDto> reissue(@Validated @RequestBody JwtTokenReissueRequest request) {
         JwtTokenDto response = jwtService.reissueToken(request.refreshToken());
         return CommonResponse.createSuccess(JWT_TOKEN_REISSUE_SUCCESS.getMessage(), response);
     }
