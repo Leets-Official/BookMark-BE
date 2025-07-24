b7f0546585cca72a3de67a2c36453fdc9550dea9
SeolA-Lee <dlcodus1201@naver.com>
2025-07-24 15:12:57 +0900

    chore: OAuth2ErrorCode 컨벤션에 맞도록 수정

--- src/main/java/leets/bookmark/global/auth/oauth2/application/exception/OAuth2ErrorCode.java
+++ src/main/java/leets/bookmark/global/auth/oauth2/application/exception/OAuth2ErrorCode.java
@@ -4,17 +4,15 @@ import leets.bookmark.global.common.exception.ErrorCode;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
-import static org.springframework.http.HttpStatus.*;
-
 @Getter
 @RequiredArgsConstructor
 public enum OAuth2ErrorCode implements ErrorCode {
 
     // OAuth2 전체 관련
-    OAUTH2_USER_INFO_EXCEPTION(BAD_REQUEST.value(), "email 정보가 없습니다."),
+    OAUTH2_USER_INFO_EXCEPTION(400, "email 정보가 없습니다."),
 
     // Kakao 관련
-    KAKAO_USER_NOT_FOUND_EXCEPTION(NOT_FOUND.value(), "해당 Kakao 사용자를 찾을 수 없습니다."),
+    KAKAO_USER_NOT_FOUND_EXCEPTION(400, "해당 Kakao 사용자를 찾을 수 없습니다."),
     KAKAO_TOKEN_REFRESH_EXCEPTION(400, "카카오 토큰 갱신을 실패하였습니다"),
     KAKAO_AUTHORIZATION_CODE_INVALID_EXCEPTION(400, "카카오 인가 코드가 유효하지 않습니다."),
     KAKAO_ACCESS_TOKEN_INVALID_EXCEPTION(400, "카카오 엑세스 토큰이 유효하지 않습니다."),
@@ -22,5 +20,4 @@ public enum OAuth2ErrorCode implements ErrorCode {
 
     private final int errorCode;
     private final String message;
-
 }
