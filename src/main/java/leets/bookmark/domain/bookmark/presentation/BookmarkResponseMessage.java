606aa6f46aa1311326aa8fd2897f800b7d2141d0
choes0101 <166037587+choes0101@users.noreply.github.com>
2025-07-24 17:21:39 +0900

    refactor: BookmarkResponseMessage 추가

--- src/main/java/leets/bookmark/domain/bookmark/presentation/BookmarkResponseMessage.java
+++ src/main/java/leets/bookmark/domain/bookmark/presentation/BookmarkResponseMessage.java
@@ -8,7 +8,8 @@ import lombok.AllArgsConstructor;
 public enum BookmarkResponseMessage {
     BOOKMARK_SEARCH_SUCCESS("북마크 검색에 성공했습니다."),
     BOOKMARK_MEMO_SEARCH_SUCCESS("북마크 메모 검색에 성공했습니다."),
-    BOOKMARK_FILTER_SUCCESS("북마크 필터링에 성공했습니다.");
+    BOOKMARK_FILTER_SUCCESS("북마크 필터링에 성공했습니다."),
+    BOOKMARK_PREVIEW_SUCCESS("북마크 미리보기에 성공했습니다.");
 
     private final String message;
 }
