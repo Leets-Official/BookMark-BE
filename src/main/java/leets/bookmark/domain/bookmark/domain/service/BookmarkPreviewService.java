e74ba91edd4a86407cd303a61aca5c528dfcefa6
choes0101 <166037587+choes0101@users.noreply.github.com>
2025-07-25 23:52:25 +0900

    refactor: 미리보기 매퍼 추가

--- src/main/java/leets/bookmark/domain/bookmark/domain/service/BookmarkPreviewService.java
+++ src/main/java/leets/bookmark/domain/bookmark/domain/service/BookmarkPreviewService.java
@@ -1,7 +1,10 @@
 package leets.bookmark.domain.bookmark.domain.service;
 
+import lombok.RequiredArgsConstructor;
+
 import leets.bookmark.domain.bookmark.application.dto.response.BookmarkResponse;
 import leets.bookmark.domain.bookmark.application.dto.response.BookmarkPreviewResponse;
+import leets.bookmark.domain.bookmark.application.mapper.BookmarkPreviewMapper;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -10,24 +13,19 @@ import java.util.List;
 import java.util.Map;
 
 @Service
+@RequiredArgsConstructor
 public class BookmarkPreviewService {
 
   private final RestTemplate restTemplate;
+  private final BookmarkPreviewMapper bookmarkPreviewMapper;
 
   @Value("${preview.api.url}")
   private String previewApiUrl;
 
-  public BookmarkPreviewService(RestTemplate restTemplate) {
-    this.restTemplate = restTemplate;
-  }
 
   public List<BookmarkResponse> extractPreviewFromUrl(String url) {
     BookmarkPreviewResponse response = callPreviewApi(url, previewApiUrl);
-    BookmarkResponse preview = BookmarkResponse.builder()
-        .title(response.title())
-        .thumbnailUrl(response.thumbnailUrl())
-        .faviconUrl(response.faviconUrl())
-        .build();
+    BookmarkResponse preview = bookmarkPreviewMapper.toResponse(response);
     return List.of(preview);
   }
 
