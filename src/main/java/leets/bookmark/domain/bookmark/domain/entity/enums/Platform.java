f8ad611b819f9959593e98a79d843bbfe2ccbd6d
jj <syeonie0526@gmail.com>
2025-07-31 15:35:56 +0900

    refactor: Platform enum에 @RequiredArgsConstructor 추가

--- src/main/java/leets/bookmark/domain/bookmark/domain/entity/enums/Platform.java
+++ src/main/java/leets/bookmark/domain/bookmark/domain/entity/enums/Platform.java
@@ -1,7 +1,9 @@
 package leets.bookmark.domain.bookmark.domain.entity.enums;
 
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
+@RequiredArgsConstructor
 @Getter
 public enum Platform {
     NAVER("네이버"),
@@ -14,7 +16,4 @@ public enum Platform {
 
     private final String platformName;
 
-    Platform(String platformName) {
-        this.platformName = platformName;
-    }
 }
