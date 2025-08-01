2dd66720c5ace20de9dd4580529af520ac5059f7
SeolA-Lee <dlcodus1201@naver.com>
2025-08-01 22:29:42 +0900

    chore: 에러코드 수정

--- src/main/java/leets/bookmark/domain/category/application/exception/CategoryErrorCode.java
+++ src/main/java/leets/bookmark/domain/category/application/exception/CategoryErrorCode.java
@@ -12,7 +12,7 @@ public enum CategoryErrorCode implements ErrorCode {
     CATEGORY_NOT_FOUND_EXCEPTION(404, "카테고리를 찾을 수 없습니다."),
     CATEGORY_OWNER_MISMATCH_EXCEPTION(403, "해당 카테고리 소유자만 접근 가능합니다."),
     CATEGORY_LIMIT_EXCEED_EXCEPTION(400, "카테고리는 최대 15개까지 생성 가능합니다."),
-    CATEGORY_HAS_BOOKMARK_EXCEPTION(404, "해당 카테고리에 북마크가 존재합니다.");
+    CATEGORY_HAS_BOOKMARK_EXCEPTION(400, "해당 카테고리에 북마크가 존재합니다.");
 
     private final int errorCode;
     private final String message;
