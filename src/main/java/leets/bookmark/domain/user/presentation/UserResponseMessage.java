11fdb43fc88fe37510a90d9bf014b1a42a0102b1
SeolA-Lee <dlcodus1201@naver.com>
2025-08-02 15:20:00 +0900

    chore: 마침표 추가

--- src/main/java/leets/bookmark/domain/user/presentation/UserResponseMessage.java
+++ src/main/java/leets/bookmark/domain/user/presentation/UserResponseMessage.java
@@ -8,7 +8,7 @@ import lombok.RequiredArgsConstructor;
 public enum UserResponseMessage {
 
     GET_USER_INFO_SUCCESS("정보 조회에 성공했습니다."),
-    UPDATE_USER_NICKNAME_SUCCESS("닉네임 변경에 성공했습니다"),
+    UPDATE_USER_NICKNAME_SUCCESS("닉네임 변경에 성공했습니다."),
     WITHDRAW_USER_SUCCESS("정상적으로 탈퇴되었습니다.");
 
     private final String message;
